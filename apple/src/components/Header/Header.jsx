7d2e92df00d9b824d3bced11b21f169217c022e5
Nurcahanefendizade19 <nunu.wwork@gmail.com>
2026-02-27 09:02:08 +0400

    header bitdi

--- apple/src/components/Header/Header.jsx
+++ apple/src/components/Header/Header.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import './Header.css'
+import iphone from "../../assets/iphone17 .png";
+
 
 const Header = () => {
   return (
-    <div>Header</div>
+    < >
+    
+        <div className='hdr'>Products, services and OS functions may not be available in this country. </div>
+<div>
+  <nav className='navcon1'>
+    <ul>
+      <li><i class="fa-brands fa-apple"></i></li>
+            <li className='menu-item'>Mac</li>
+      <li className='menu-item'>Ipad</li>
+      <li className='menu-item'>Iphone</li>
+      <li className='menu-item'>Watch</li>
+      <li className='menu-item'>AirPods</li>
+      <li className='menu-item'>Tv & Home</li>
+      <li className='menu-item'>Entertainment</li>
+      <li className='menu-item'>Support</li>
+      <li className='menu-item'>Where to Buy</li>
+
+    </ul>
+  </nav>
+</div>
+
+<div className='ipcon'>
+<h1>IPhone</h1>
+<h3>Say hello to the latest geneeration pf iphone</h3>
+<h4>Learn more</h4>
+</div>
+
+<div className='iphoneImg'>
+
+<img className='iphone' src={iphone} alt="" />
+
+</div>
+
+
+    
+    </>
   )
 }
 
